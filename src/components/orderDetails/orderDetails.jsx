e5233175ba508368a30b58ecf4ff4ab254275d51
Stanislav <stasikparmut@yandex.ru>
2022-09-18 08:30:37 +0300

    ready for first review

--- src/components/orderDetails/orderDetails.jsx
+++ src/components/orderDetails/orderDetails.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import orderDoneImg from "../../images/done.png";
 import orderDetailsStyles from "./orderDetails.module.css";
 
@@ -9,7 +7,7 @@ const OrderDetails = () => {
     <div className={`${orderDetailsStyles.container} pt-15`}>
       <h1 className="text text_type_digits-large mb-10">034536</h1>
       <h2 className="text text_type_main-medium mb-15">Идентификатор заказа</h2>
-      <img src={orderDoneImg} alt="значок заказ принят" />
+      <img src={orderDoneImg} alt="заказ принят" />
       <span className="text text_type_main-default mt-15 mb-3">
         Ваш заказ начали готовить
       </span>
